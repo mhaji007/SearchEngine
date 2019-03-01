a4819fef07c83df08e0d95b3f3554630511db712
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-01 15:46:38 -0500

    Show title, description and url for site results

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -41,8 +41,26 @@ class SiteResultsProvider {
 
 		while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 
+			$id = $row["id"];
+			$url = $row["url"];
 			$title = $row["title"];
-			$resultsHtml .="$title <br>";
+			$description = $row["description"];
+
+			$resultsHtml .= "<div class='resultContainer'>
+
+							<h3 class='title'>
+								<a class='result' href='$url'
+								$title
+								</a>
+							</h3>
+							<span class='url'>$url</span>
+							<span class='description'>$description</span>
+
+
+							</div>";
+
+
+			//$resultsHtml .="$title <br>";
 		}
 
 		$resultsHtml .= "</div>";

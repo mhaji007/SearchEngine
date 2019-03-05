0c0d9bca467ef1a881b7e7dac30c0b298999fe05
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-05 10:17:03 -0500

    Rename the database, add comments

--- crawl.php
+++ crawl.php
@@ -2,8 +2,13 @@
 include("config.php");
 include("classes/DomDocumentParser.php");
 
+// array to  contain all the links already visited
 $alreadyCrawled = array();
+
+// array to hold links still need to be crawled
 $crawling = array();
+
+// array to hold images aleady visited
 $alreadyFoundImages = array();
 
 function linkExists($url) {
@@ -47,7 +52,7 @@ function insertImage($url, $src, $alt, $title) {
 
 }
 
-
+// function to convert relative links to absolute links (full url)
 function createLink($src, $url) {
 
 	/*
@@ -65,25 +70,27 @@ function createLink($src, $url) {
 
 	*/	
 
-	$scheme =  parse_url($url)["scheme"] ; // http or https
+	//parse_url is a built-in php function
+	$scheme =  parse_url($url)["scheme"]; // http or https
 	$host = parse_url($url)["host"]; // www.bbc.com
 
+	// if first two characters are //
 	if(substr($src, 0, 2) == "//") {
 		$src = $scheme . ":" .$src;
 	}
-
+	//if first character is a /
 	else if (substr($src,0, 1) == "/") {
 		$src = $scheme . "://" . $host . $src;
 	}
-
+	// if first two characters are ./
 	else if (substr($src,0, 2) == "./") {
 		$src = $scheme . "//" . $host . dirname(parse_url($url)["path"]) . substr($src,1);
 	}
-
+	// if first three characters are ../
 	else if (substr($src,0,3) == "../") {
-		$src = $scheme . "://" / $host . "/" . $src;
+		$src = $scheme . "://" . $host . "/" . $src;
 	}
-
+	// if first 5 characters are not https or first four characters are not http 
 	else if (substr($src,0,5) != "https" && substr($src,0,4) != "http") {
 		$src = $scheme . "://" . $host . "/" . $src;
 	}
@@ -96,26 +103,32 @@ function createLink($src, $url) {
 	*/
 }
 
+// function to retrun the details of urls
 function getDetails($url) {
 
 	global $alreadyFoundImages;
 
 	$parser = new DomDocumentParser($url);
 
+	// returns an array of titles
 	$titleArray = $parser->getTitleTags();
 
+	// if size of the array is zero or the first item in the array is null, return
 	if (sizeof($titleArray) == 0 || $titleArray->item(0) == NULL) {
 		return;
 	}
 	
+	// return the value of the first item of the array
 	$title = $titleArray->item(0)->nodeValue;
-	
+
+	// replace new lines with empty strings (delete any new lines)
 	$title = str_replace("\n","",$title);
 
+	// if there are no titles we don't want them in our database. Google penalizes websites that do not have titles or proper keywords as well. If there are no titles we don't want to crawl them. There are alternatives. e.g., you could show the url instead of title.
 	if($title == "") {
 		return;
 	}
-
+	// Although we don't crawl websites with no title tage, we do crawl websites with no description or keywords tag. We initialize them with empty strings
 	$description = "";
 	$keywords = "";
 
@@ -128,7 +141,7 @@ function getDetails($url) {
 		}
 
 		if($meta->getAttribute("name") == "keywords") {
-			$keyworsds = $meta->getAttribute("content");
+			$keywords = $meta->getAttribute("content");
 		}
 
 	}
@@ -136,6 +149,7 @@ function getDetails($url) {
 	$description = str_replace("\n","",$description);
 	$keywords = str_replace("\n","",$keywords);
 
+	// for testing
 	//echo "URL: $url, Title: $title, Description: $description, Keywords: $keywords<br>";
     
 	if(linkExists($url)) {
@@ -181,18 +195,22 @@ function getDetails($url) {
 
 }
 
+// function to recursively follow links on the webpage
 function followLinks($url) {
 
+	// global references to arrays
 	global $alreadyCrawled;
 	global $crawling;
 
 	$parser = new DomDocumentParser($url);
 
+	// retrieves all the links on the specified url
 	$linkList = $parser->getlinks();
 
+	// loops over each link and gets the href attribute 
 	foreach($linkList as $link) {
 		$href = $link->getAttribute("href");
-
+		// ignore the hrefs that have # to use the behavior of the links (they are not real links)
 		if(strpos($href,"#") !== false) {
 			continue;
 		}
@@ -208,34 +226,39 @@ function followLinks($url) {
 				}
 		}
 	*/	
-
+		// ignore href chatracters 0-11 that have javascript value. Javascript could be used in links to execute some commands. These are for our intents and purposes are not proper links. 
 		else if(substr($href,0,11) == "javascript:") {
 			continue;
 		}
 
+		// creates absolute links from relative links and stores them in $href
 		$href = createLink($href, $url);
-
+		// here is where the recursive visting of links is accomplished
 		if (!in_array($href, $alreadyCrawled)) {
-			$alreadyCrawled[] = $href;
+			$alreadyCrawled[] = $href; // put it at the next item same as array_push($alreadyCrawled, $href);
 			$crawling[] = $href;
 
-			// insert href
+			// insert href into the database
 			getDetails($href);
 		}
 
+		// to speed things up when testing. If there are any duplicates in the alreadyCrawled return
 		//else return;
 
 
 		//echo $href . "<br>";
 	}
 
+	// take the next item of the array
 	array_shift($crawling);
 
+	// go over every item of the crawling list
 	foreach($crawling as $site) {
+		// recursive call to the function
 		followLinks($site);
 	}
 }
 
-$startUrl = "https://www.bbc.com";
+$startUrl = "http://www.apple.com";
 followLinks($startUrl);
 ?>

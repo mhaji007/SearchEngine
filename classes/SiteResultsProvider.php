743ccbc303a8048aa51422a9d12f40219f037dd7
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-05 18:50:32 -0500

    Fix titles not visible in the search results, trim the result texts, add/fix comments

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -7,6 +7,7 @@ class SiteResultsProvider {
 		$this->con = $con;
 	}
 
+	// function to return the number of results found
 	public function getNumResults($term) {
 
 		$query = $this->con->prepare("SELECT COUNT(*) as total 
@@ -15,15 +16,18 @@ class SiteResultsProvider {
 										 OR keywords LIKE :term 
 										 OR description LIKE :term");
 
+		// check for any number of character before and after the term. Should be done before putting in the bind funciton. $query->bindParam(":term", "%" . $term . "%);  would not work.
 		$searchTerm = "%". $term . "%";
 		$query->bindParam(":term", $searchTerm);
 		$query->execute();
 
+	// store the results in an associative array (key, value array)
 		$row = $query->fetch(PDO::FETCH_ASSOC);
 		return $row["total"];
 
 	}
-
+	// function to ouput the results
+	// page is the current page we are on. Page size is the number of results per page.
 	public function getResultsHtml($page, $pageSize, $term) {
 
 		$query = $this->con->prepare("SELECT * 
@@ -37,37 +41,48 @@ class SiteResultsProvider {
 		$query->bindParam(":term", $searchTerm);
 		$query->execute();
 
-		$resultsHtml = "<div class ='siteResults'>";
 
-		while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+		$resultsHtml = "<div class='siteResults'>";
 
+
+		while($row = $query->fetch(PDO::FETCH_ASSOC)) {
 			$id = $row["id"];
 			$url = $row["url"];
 			$title = $row["title"];
 			$description = $row["description"];
 
+			$title = $this->trimField($title, 55);
+			$description = $this->trimField($description, 230);
+			
 			$resultsHtml .= "<div class='resultContainer'>
 
-							<h3 class='title'>
-								<a class='result' href='$url'
-								$title
-								</a>
-							</h3>
-							<span class='url'>$url</span>
-							<span class='description'>$description</span>
-
+								<h3 class='title'>
+									<a class='result' href='$url'>
+										$title
+									</a>
+								</h3>
+								<span class='url'>$url</span>
+								<span class='description'>$description</span>
 
 							</div>";
 
+		//$resultsHtml .="$title <br>";
 
-			//$resultsHtml .="$title <br>";
 		}
+
 
 		$resultsHtml .= "</div>";
 
 		return $resultsHtml;
-
 	}
+
+	// function to trim the result text
+	private function trimField($string, $characterLimit) {
+
+		$dots = strlen($string) > $characterLimit ? "..." : "";
+		return substr($string, 0, $characterLimit) . $dots;
+	}
+
 
 
 

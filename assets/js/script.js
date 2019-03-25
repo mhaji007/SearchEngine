ca52c70634ef267aa2aee34254086029b39b095d
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-25 19:53:55 -0400

    Update the clicks value via ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$(".result").on("click", function () {
-		// console.log("I was clicked");
 
-		//get the value of the href
+	$(".result").on("click", function() {
+		
 		var id = $(this).attr("data-linkId");
 		var url = $(this).attr("href");
-		//console.log(id);
 
 		if(!id) {
 			alert("data-linkId attribute not found");
@@ -14,14 +12,88 @@ $(document).ready(function(){
 
 		increaseLinkClicks(id, url);
 
-
-
-		return false;  // don't do default behavior (here, don't go to the page clicked)
+		//return false;
 	});
+
+
 });
+
 
 function increaseLinkClicks(linkId, url) {
 
+	$.post("ajax/updateLinkCount.php", {linkId: linkId})
+	.done(function(result) {
+		if(result != "") {
+			alert(result);
+			return;
+		}
 
+		window.location.href = url;
+
+	});
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+
+// 	$(".result").on("click", function () {
+// 		// console.log("I was clicked");
+
+// 		//get the value of the href
+// 		var id = $(this).attr("data-linkId");
+// 		var url = $(this).attr("href");
+// 		//console.log(id);
+
+// 		if(!id) {
+// 			alert("data-linkId attribute not found");
+// 		}
+
+// 		increaseLinkClicks(id, url);
+
+
+
+// 		return false;  // don't do default behavior (here, don't go to the page clicked)
+// 	});
+// });
+
+// function increaseLinkClicks(linkId, url) {
+
+// 	$.post("ajax/updateLinkCount.php", {linkId: linkId});
+
+// }

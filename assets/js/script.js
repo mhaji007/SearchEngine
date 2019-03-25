ee82ce762cb67f6e827df4d728c4e1d6c8667a4e
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-25 18:57:40 -0400

    Get the url of the clicked link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-	console.log("hello");
+
+	$(".result").on("click", function () {
+		// console.log("I was clicked");
+
+		//get the value of the href
+		var url = $(this).attr("href");
+		console.log(url);
+
+
+
+		return false;  // don't do default behavior (here, don't go to the page clicked)
+	});
 });
+
+function increaseLinkClicks(linkId, url) {
+
+
+
+}
